a45c097272c38a1d0621b9499277a69b42e09394
holod-e <holodniak.e@gmail.com>
2017-07-12 18:26:43 +0300

    work on articles video page

---
+++ wp-content/themes/talpis/articlesandvideo.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: статьи и видео (articlesandvideo.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+get_header(); // подключаем header.php ?>
+	<!-- SECTION BLUEHEADER-->
+	<section class="blueheader"></section>
+	<?php get_template_part('partials/breadcrumbs'); ?>
+	<div class="section-postandvideo">
+		<div class="container">
+			<?php get_template_part('partials/subscribeform'); ?>
+			<!-- FILTERS -->
+			<div class="filters"></div>
+			<div class="row">
+				<div class="col-md-8">
+					<?php $loop = new WP_Query( array( 'post_type' => 'post') );
+							if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+							<?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
+							<div class="post-wrap">
+								<p class="post-title"><?php the_title(); ?></p>
+								<a href="#modal-contact" class="playbtn" data-video='youtube' data-srcvideo="<?php echo $field = get_field('videolink');?>"></a>
+								</div>
+							</div>
+					<?php endwhile; ?>
+					<?php endif; ?>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); // подключаем footer.php ?>
--- wp-content/themes/talpis/category.php
+++ wp-content/themes/talpis/category.php
@@ -1,23 +1,29 @@
 <?php
 /**
- * Шаблон рубрики (category.php)
+ * Шаблон статьи и видео (category.php)
  * @package WordPress
  * @subpackage your-clean-template-3
  */
-get_header(); // подключаем header.php ?> 
-<section>
-	<div class="container">
-		<div class="row">
-			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
-				<h1><?php single_cat_title(); // название категории ?></h1>
-				<?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
-					<?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
-				<?php endwhile; // конец цикла
-				else: echo '<p>Нет записей.</p>'; endif; // если записей нет, напишим "простите" ?>	 
-				<?php pagination(); // пагинация, функция нах-ся в function.php ?>
+get_header(); // подключаем header.php ?>
+	<!-- SECTION BLUEHEADER-->
+	<section class="blueheader"></section>
+	<?php get_template_part('partials/breadcrumbs'); ?>
+	<div class="section-postandvideo">
+		<div class="container">
+			<?php get_template_part('partials/subscribeform'); ?>
+			<!-- FILTERS -->
+			<div class="filters"></div>
+			<div class="row">
+				<div class="col-md-8">
+					<h3><?php single_cat_title(); // название категории ?></h3>
+					<?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
+						<?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
+					<?php endwhile; // конец цикла
+					else: echo '<p>Нет записей.</p>'; endif; // если записей нет, напишим "простите" ?>
+					<?php pagination(); // пагинация, функция нах-ся в function.php ?>
+				</div>
+				<div class="col-md-4"></div>
 			</div>
-			<?php get_sidebar(); // подключаем sidebar.php ?>
 		</div>
 	</div>
-</section>
 <?php get_footer(); // подключаем footer.php ?>
