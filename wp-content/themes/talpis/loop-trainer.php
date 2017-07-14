4cabde00f8893b9aa874740d66932c1928f2f007
holod-e <holodniak.e@gmail.com>
2017-07-14 12:15:09 +0300

    add trainers post-type

--- wp-content/themes/talpis/loop-trainer.php
+++ wp-content/themes/talpis/loop-trainer.php
@@ -7,14 +7,19 @@
 ?>
 <div class="trainer" id="trainer-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="trainer-info">
-		<div class="trainer-photo"></div>
-		<p class="trainer-name">John Campbell</p>
-		<p class="trainer-prof">Compatible Inkjet Cartridge</p>
+		<div class="trainer-photo">
+      <?php
+      $image = get_field('trainer-photo');
+      if( !empty($image) ): ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      <?php endif; ?>
+    </div>
+		<p class="trainer-name"><?php the_field('trainer-name'); ?></p>
+		<p class="trainer-prof"><?php the_field('trainer-prof'); ?></p>
 	</div>
 	<div class="trainer-text">
-		<p class="text-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
-		<div class="text-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-		<a
-			class="totrainer-site">Перейти на сайт тренера ></a>
+		<p class="text-title"><?php the_title(); ?> </p>
+		<div class="text-txt"><?php the_field('trainer-about');?></div>
+		<a class="totrainer-site" href="<?php the_field('trainer-site');?>">Перейти на сайт тренера</a>
   </div>
 </div>
