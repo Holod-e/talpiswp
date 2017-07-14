baa175cf500f0740014cb21f8e9617104da9ef8b
holod-e <holodniak.e@gmail.com>
2017-07-14 11:33:05 +0300

    add some files

---
+++ wp-content/themes/talpis/loop-trainer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Цикл тренеров (loop-trainer.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+?>
+<div class="trainer" id="trainer-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="trainer-info">
+		<div class="trainer-photo"></div>
+		<p class="trainer-name">John Campbell</p>
+		<p class="trainer-prof">Compatible Inkjet Cartridge</p>
+	</div>
+	<div class="trainer-text">
+		<p class="text-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+		<div class="text-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+		<a
+			class="totrainer-site">Перейти на сайт тренера ></a>
+  </div>
+</div>
